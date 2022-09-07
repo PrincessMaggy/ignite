9653a2c5def27e0cce3ae937a02e9b7b461d3604
PrincessMaggy <edoziemagdalene@gmail.com>
2022-09-07 10:50:44 +0100

    fetched other data

--- src/actions/gamesAction.js
+++ src/actions/gamesAction.js
@@ -1,16 +1,22 @@
 import axios from "axios";
 
-import { popularGamesURL } from "../api";
+import { popularGamesURL, upcomingGamesURL, newGamesURL} from "../api";
 
 // action creator
 
 export const loadGames = () => async (dispatch)=> {
 // fetch axios
     const popularData = await axios.get(popularGamesURL());
+    const upcomingData = await axios.get(upcomingGamesURL());
+    const newGamesData = await axios.get(newGamesURL());
+
+
     dispatch({
         type: "FETCH_GAMES",
         payload: {
             popular: popularData.data.results,
+            upcoming: upcomingData.data.results,
+            newGames: newGamesData.data.results
         },
     });
 };
--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 const baseUrl ="https://api.rawg.io/api/";
-const api_key = "824e421e1add4ebf9b4ddfdc631267a9";
+// const api_key = "824e421e1add4ebf9b4ddfdc631267a9";
 
 // get current month
 const getCurrentMonth = () =>{
@@ -33,5 +33,14 @@ const nextYear=  `${currentYear+1}-${currentMonth}-${currentDay}`;
 
 // popular games
 const popular_games = `games?key=824e421e1add4ebf9b4ddfdc631267a9&dates=${lastYear},${currentDate}&ordering=-rating&page_size=20`;
+const upcoming_games = `games?key=824e421e1add4ebf9b4ddfdc631267a9&dates=${currentDate},${nextYear}&ordering=-added&page_size=20`;
+const newGames =  `games?key=824e421e1add4ebf9b4ddfdc631267a9&dates=${lastYear},${currentDate}&ordering=-released&page_size=20`;
+
+
+
 
 export const popularGamesURL = () => `${baseUrl}${popular_games}`;
+
+export const upcomingGamesURL =() => `${baseUrl}${upcoming_games}`;
+
+export const newGamesURL = () => `${baseUrl}${newGames}`;
