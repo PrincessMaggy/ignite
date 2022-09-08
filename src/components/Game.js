78d2e79e755e72b78499d2249090710a4860e12a
PrincessMaggy <edoziemagdalene@gmail.com>
2022-09-08 15:35:14 +0100

    styled cards

--- src/components/Game.js
+++ src/components/Game.js
@@ -8,12 +8,24 @@ import { motion } from "framer-motion";
 const Game =(props) =>{
 
     return(
-        <div>
+        <StyledGame>
             <h3>{props.name}</h3>
             <p>{props.released}</p>
             <img src={props.image} alt={props.name} />
-        </div>
+        </StyledGame>
     );
 };
 
+
+const StyledGame = styled(motion.div)`
+    min-height: 30vh;
+    box-shadow: 0px 5px 30px rgba(0,0,0,0.1);
+    text-align: center;
+    border-radius: 1rem;
+    img{
+        width: 100%;
+        height: 40vh;
+        object-fit: cover;
+    }
+`
 export default Game;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -22,7 +22,6 @@ const {popular, newGames, upcoming} = useSelector((state) => state.games)
 
     return(
         <GameList>
-            <h2>Welcome Home</h2>
             <Games>
                 {upcoming.map((game)=> 
                 <Game 
@@ -39,8 +38,17 @@ const {popular, newGames, upcoming} = useSelector((state) => state.games)
 };
 
 const GameList = styled(motion.div)`
+    padding: 0rem 5rem;
+    h2{
+        padding: 5rem 0rem;
+    }
 `
 const Games = styled(motion.div)`
+    min-height: 80vh;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
+    grid-column-gap: 3rem;
+    grid-row-gap: 5rem;
 `
 
 export default Home;
