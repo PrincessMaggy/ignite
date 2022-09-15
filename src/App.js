43b28a5290b25b9f2b1ad3e4e653acad4f17216f
PrincessMaggy <edoziemagdalene@gmail.com>
2022-09-15 10:10:29 +0100

    Created Nav component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Nav from "./components/Nav";
+
 // components and pages
 import Home from "./pages/Home";
 import GlobalStyles from "./components/GlobalStyles";
@@ -9,6 +11,7 @@ function App() {
   return (
     <div className="App">
       <GlobalStyles/>
+      <Nav />
       <Routes>
         <Route path="/ignite" element={ <Home/>}></Route>
         <Route path="/game/:id" element={ <Home/>}></Route>
